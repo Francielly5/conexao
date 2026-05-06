69f098ded71d288594180f00368585370856431e
Francielly5 <franciellym.ferreira15@gmail.com>
2026-05-05 22:09:19 -0300

    Conexao edu 05.05

---
+++ conexao.php
@@ -0,0 +1,16 @@
+<?php
+$host="localhost";
+$banco="biblioteca2";
+$usuario="root";
+$senha="usbw";
+
+try{
+    $pdo = new PDO("mysql:host=$host;dbname=$banco;charset=utf8",$usuario, $senha);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+}catch(PDOException $e){
+    die("Erro ao conectar: ".$e->getMessage());
+}
+
+
+
+?>
